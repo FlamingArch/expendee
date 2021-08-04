7b0651301016a3872c04b111e749f5abef5f2e18
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-08-05 01:07:56 +0530

    Added Firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import anime from "animejs/lib/anime.es.js";
+import firebase from "firebase/firebase-app";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDgUKc_MxIehNI37s51TxYJ2ZD-IJgXQEI",
+  authDomain: "expendee-f303.firebaseapp.com",
+  projectId: "expendee-f303",
+  storageBucket: "expendee-f303.appspot.com",
+  messagingSenderId: "44443531028",
+  appId: "1:44443531028:web:b75535dd9ab294bc9bf401",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+console.log('firebase :>> ', firebase);
 
 ReactDOM.render(
   <React.StrictMode>

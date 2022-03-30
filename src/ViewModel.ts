8069bf35a573d000b3fa7c7e4769bd07aa1b870b
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-03-30 14:35:11 +0530

    Added Models, ViewModel

---
+++ src/ViewModel.ts
@@ -0,0 +1,41 @@
+import Transaction, { TransactionType } from "./Model/Transaction";
+import Wallet from "./Model/Wallet";
+
+class ViewModel {
+  wallets: Wallet[] = [];
+
+  constructor() {}
+
+  getAllTransactions() {
+    let transactions = [];
+
+    this.wallets.forEach((wallet) => {
+      transactions = transactions.concat(wallet.transactions);
+    });
+
+    transactions.sort(
+      (a: Transaction, b: Transaction) =>
+        a.dateTime.getTime() - b.dateTime.getTime()
+    );
+
+    return transactions;
+  }
+
+  getAllSpentTransactions(transactions?: Transaction[]) {
+    return (transactions ?? this.getAllTransactions()).filter(
+      (e: Transaction) => e.type === TransactionType.expense
+    );
+  }
+
+  getAllReceivedTransactions(transactions?: Transaction[]) {
+    return (transactions ?? this.getAllTransactions()).filter(
+      (e: Transaction) => e.type === TransactionType.income
+    );
+  }
+
+  getWalletTransactions(wallet: Wallet, transactions?: Transaction[]) {
+    return (transactions ?? this.getAllTransactions()).filter(
+      (e: Transaction) => e.wallet.id === wallet.id
+    );
+  }
+}

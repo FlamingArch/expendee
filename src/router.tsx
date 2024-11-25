d56a986eefa878b40d7a44915a278d7ec294c8c7
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2024-11-25 16:46:27 +0530

    Added Zustand for State Management

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import PageHome from './routes/Home'
 
---
+++ src/store.ts
@@ -0,0 +1,7 @@
+import { create } from "zustand";
+
+type AppStoreProps = {};
+
+const useAppStore = create<AppStoreProps>((set) => ({}));
+
+export default useAppStore;

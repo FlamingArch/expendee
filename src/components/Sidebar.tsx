eaad68cc1e11a5c65ce54d35a99d6ceb93333e14
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2025-03-20 01:22:22 +0530

    New TabBar Styles, Sidebar Tabs

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,13 +2,14 @@ type Props = {
   children: React.ReactNode;
   width?: number;
   collapsed?: boolean;
+  className?: string;
 };
 
 export default function Sidebar(props: Props) {
   return (
     <aside
       style={{ width: props.collapsed ? 0 : props.width ?? "280px" }}
-      className="flex flex-col border-r border-gray-900"
+      className={"flex flex-col border-r border-gray-900 " + props.className}
     >
       {props.children}
     </aside>
--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,28 +1,66 @@
 type TabBarItem = {
   label: string;
   icon?: (props: any) => React.ReactNode;
+  secondaryLabel?: string;
+  tag?: string;
 };
+
+type TabBarStyles =
+  | "segmented"
+  | "sidebar"
+  | "sidebarGrouped"
+  | "sidebarProminent"
+  | "sidebarProminentGrouped";
 
 type Props = {
   items: TabBarItem[];
-  selected: number;
-  onChange: (index: number) => void;
+  selected: number | string;
+  onChange: (value: any) => void;
+  type: TabBarStyles;
   className?: string;
+};
+
+const styles = {
+  segmented: {
+    base: "grid grid-flow-col w-full bg-accent/10 dark:bg-accent/25 rounded-xl cursor-pointer",
+    tab: "grow flex items-center w-auto text-accent rounded-xl text-sm text-center gap-3 cursor-pointer p-2",
+    selected: "bg-accent text-white",
+  },
+  sidebar: {
+    base: "grid gap-3 w-full rounded-xl text-white cursor-pointer",
+    tab: "grow flex items-center w-auto bg-accent-dimmed/20 rounded-xl text-sm gap-3 cursor-pointer p-3",
+    selected: "bg-accent/100 text-white",
+  },
+  sidebarGrouped: {
+    base: "grid w-full rounded-xl bg-accent-dimmed/20 text-white cursor-pointer",
+    tab: "grow flex items-center w-auto rounded-xl text-sm gap-3 cursor-pointer p-3",
+    selected: "bg-accent/100 text-white",
+  },
+  sidebarProminent: {
+    base: "grid gap-3 w-full rounded-xl text-white cursor-pointer",
+    tab: "grow flex items-center w-auto bg-accent-dimmed/20 rounded-xl text-sm gap-3 cursor-pointer p-3",
+    selected: "bg-white text-black shadow-lg",
+  },
+  sidebarProminentGrouped: {
+    base: "grid w-full bg-accent-dimmed/20 rounded-xl text-white cursor-pointer",
+    tab: "grow flex items-center w-auto rounded-xl text-sm gap-3 cursor-pointer p-3",
+    selected: "bg-white text-black shadow-lg",
+  },
 };
 
 export default function TabBar(props: Props) {
   return (
-    <div
-      className={`grid grid-flow-col w-full bg-accent/10 rounded-xl ${props.className}`}
-    >
+    <div className={`${styles[props.type].base} ${props.className}`}>
       {props.items &&
         props.items.map((item, index) => (
           <Tab
-            key={index}
-            selected={index === props.selected}
+            type={props.type}
+            key={item.tag ?? index}
+            selected={(item.tag ?? index) === props.selected}
             label={item.label}
+            secondaryLabel={item.secondaryLabel}
             icon={item.icon}
-            onClick={() => props.onChange(index)}
+            onClick={() => props.onChange(item.tag ?? index)}
           />
         ))}
     </div>
@@ -34,17 +72,22 @@ type TabProps = {
   onClick: () => void;
   label: string;
   icon?: (props: any) => React.ReactNode;
+  type: TabBarStyles;
+  secondaryLabel?: string;
 };
 
 function Tab(props: TabProps) {
-  let selectedStyles = props.selected ? "bg-accent text-white rounded-xl" : "";
+  let selectedStyles = props.selected ? styles[props.type].selected : "";
   return (
     <div
-      className={`grow flex items-center w-auto text-accent text-sm cursor-pointer p-2 ${selectedStyles}`}
+      className={`${styles[props.type].tab} ${selectedStyles}`}
       onClick={props.onClick}
     >
       {props.icon && <props.icon />}
-      <p className="flex-grow text-center">{props.label}</p>
+      <p className="flex-grow">{props.label}</p>
+      {props.secondaryLabel && (
+        <p className="text-xs opacity-80">{props.secondaryLabel}</p>
+      )}
     </div>
   );
 }

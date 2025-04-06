5e7a114cddf1e1c55babc11944a4b74b16525dd5
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2025-04-06 21:10:42 +0530

    Padding support in Section

--- views/Section.tsx
+++ views/Section.tsx
@@ -1,13 +1,22 @@
 type PropsSection = {
   heading?: string;
+  innerPadding?: number;
+  innerGap?: number;
   children?: React.ReactNode;
 };
 
 export default function Section(props: PropsSection) {
   return (
-    <section>
-      {props.heading && <p>{props.heading}</p>}
-      {props.children}
+    <section className="flex flex-col">
+      {props.heading && (
+        <p className="text-sm font-bold p-4 pb-0">{props.heading}</p>
+      )}
+      <div
+        style={{ padding: props.innerPadding ?? 16, gap: props.innerGap ?? 16 }}
+        className="flex flex-col "
+      >
+        {props.children}
+      </div>
     </section>
   );
 }

3e63c1f79a4b345d0062ef3e1b3668aa3822eb9d
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2025-04-06 20:22:17 +0530

    Initial Implementation of Sidebar-Content Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
-import type { Metadata } from "next";
 import { Manrope, Playfair_Display } from "next/font/google";
+import type { Metadata } from "next";
 import "./globals.css";
+
+import Page from "@/views/Page";
+import Section from "@/views/Section";
+import Tab from "@/views/Tab";
+
+import { MdWallet } from "react-icons/md";
+import { HiArrowsRightLeft } from "react-icons/hi2";
+import { IoPersonCircleOutline, IoPieChartOutline } from "react-icons/io5";
+import Button from "@/views/Button";
+import Toolbar from "@/views/Toolbar";
 
 const fontBase = Manrope({
   variable: "--font-base",
@@ -21,11 +31,57 @@ type RootLayoutProps = Readonly<{
   children: React.ReactNode;
 }>;
 
+const tabs = [
+  { path: "/overview", label: "Overview", icon: IoPieChartOutline },
+  { path: "/transactions", label: "Transactions", icon: HiArrowsRightLeft },
+];
+
+const wallets = [
+  { id: 1, label: "Kotak Bank", balance: 32_709.0 },
+  { id: 2, label: "HDFC Bank", balance: 100_000.0 },
+  { id: 3, label: "Fi Bank", balance: 2_540.0 },
+];
+
+function ToolbarSidebar() {
+  return (
+    <Toolbar>
+      <p className="font-family-[family-name:var(--font-branding)] font-black text-xl">
+        Expendee
+      </p>
+      <Button icon={IoPersonCircleOutline} />
+    </Toolbar>
+  );
+}
+
 export default function RootLayout(props: RootLayoutProps) {
   const fontStyles = `${fontBase.variable} ${fontBranding.variable} font-[family-name:var(--font-base)] antialiased`;
   return (
     <html lang="en">
       <body className={`flex w-screen h-screen overflow-hidden ${fontStyles}`}>
+        <Page toolbar={<ToolbarSidebar />}>
+          <Section heading="Pages">
+            {tabs.map((item) => (
+              <Tab
+                style="sidebar"
+                key={item.path}
+                path={item.path}
+                label={item.label}
+                icon={item.icon}
+              />
+            ))}
+          </Section>
+          <Section heading="Wallets">
+            {wallets.map((item) => (
+              <Tab
+                style="sidebar"
+                key={item.id}
+                path={`wallet/${item.id}`}
+                label={item.label}
+                icon={MdWallet}
+              />
+            ))}
+          </Section>
+        </Page>
         {props.children}
       </body>
     </html>

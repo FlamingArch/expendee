077d3b2fdb11547dfa9e67d62486256889c4236d
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2025-04-06 21:10:21 +0530

    Refactored Button

--- views/Button.tsx
+++ views/Button.tsx
@@ -1,21 +1,30 @@
-type Props = {
+type ButtonProps = {
   children?: React.ReactNode;
   icon?: (props: any) => React.ReactNode;
   className?: string;
   iconClassName?: string;
 } & React.DOMAttributes<HTMLButtonElement>;
 
-export default function Button(props: Props) {
-  const { children, icon, className, iconClassName, ...buttonProps } = props;
+type ButtonIconProps = {
+  icon: (props: any) => React.ReactNode;
+  iconClassName?: string;
+};
+
+function ButtonIcon(props: ButtonIconProps) {
+  return <props.icon className={"w-5 h-5 text-text " + props.iconClassName} />;
+}
+
+export default function Button(props: ButtonProps) {
+  const { children, icon, className, iconClassName, ...btnProps } = props;
   return (
     <button
-      {...buttonProps}
+      {...btnProps}
       className={
         "p-3 rounded-xl hover:bg-black/10 dark:hover:bg-white/10 transition-colors " +
         className
       }
     >
-      {props.icon && <props.icon className={"w-5 h-5 " + iconClassName} />}
+      {props.icon && <ButtonIcon icon={icon!} iconClassName={iconClassName} />}
       {children}
     </button>
   );

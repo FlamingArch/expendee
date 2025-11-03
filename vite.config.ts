445d7aef41cb1bf0ce6e2d20229510e489ea4cf1
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2025-11-03 18:47:34 +0530

    Switch away from src  structure, resetup TailwindCSS and Google Fonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { tanstackRouter } from "@tanstack/router-plugin/vite"
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
     tailwindcss(),
-    react(),
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
   ],
-});
+})

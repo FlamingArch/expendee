8c9e395692842608f2345388d3e413632863c20f
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2025-11-07 00:45:13 +0530

    Added back Google Fonts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,11 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: function () {
-    return <h1>Hello, World</h1>;
+    return (
+      <div className="flex flex-col gap-0 items-center justify-center flex-1">
+        <h1 className="font-branding font-bold text-2xl p-5">Expendee</h1>
+        <p>🚧 Work in Progress 🚧</p>
+      </div>
+    );
   },
 });
